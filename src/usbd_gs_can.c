d2e59efa28aaf2110e5031681e32497bfd6b2dd2
Hubert Denkmair <hubert@denkmair.de>
2016-04-15 12:45:16 +0200

    make const data structs static consts :)

--- src/usbd_gs_can.c
+++ src/usbd_gs_can.c
@@ -116,6 +116,32 @@ __ALIGN_BEGIN uint8_t USBD_GS_CAN_CfgDesc[USB_CAN_CONFIG_DESC_SIZ] __ALIGN_END =
 
 };
 
+
+// device info
+static const struct gs_device_config USBD_GS_CAN_dconf = {
+	0, // reserved 1
+	0, // reserved 2
+	0, // reserved 3
+	0, // interface count (0=1, 1=2..)
+	1, // software version
+	1  // hardware version
+};
+
+// bit timing constraints
+static const struct gs_device_bt_const USBD_GS_CAN_btconst = {
+	GS_CAN_FEATURE_LISTEN_ONLY | GS_CAN_FEATURE_LOOP_BACK, // supported features
+	48000000, // can timing base clock
+	1, // tseg1 min
+	16, // tseg1 max
+	1, // tseg2 min
+	8, // tseg2 max
+	4, // sjw max
+	1, // brp min
+	1024, //brp_max
+	1, // brp increment;
+};
+
+
 uint8_t USBD_GS_CAN_Init(USBD_HandleTypeDef *pdev, queue_t *q_frame_pool, queue_t *q_from_host)
 {
 	uint8_t ret = USBD_FAIL;
@@ -235,25 +261,6 @@ static uint8_t USBD_GS_CAN_Vendor_Request(USBD_HandleTypeDef *pdev, USBD_SetupRe
 {
 	USBD_GS_CAN_HandleTypeDef *hcan = (USBD_GS_CAN_HandleTypeDef*) pdev->pClassData;
 
-	struct gs_device_config dconf;
-	memset(&dconf, 0, sizeof(dconf));
-	dconf.icount = 0;
-	dconf.hw_version = 1;
-	dconf.sw_version = 1;
-
-	struct gs_device_bt_const btconst;
-	memset(&btconst, 0, sizeof(btconst));
-	btconst.fclk_can = 48000000;
-	btconst.tseg1_min = 1;
-	btconst.tseg1_max = 16;
-	btconst.tseg2_min = 1;
-	btconst.tseg2_max = 8;
-	btconst.sjw_max = 4;
-	btconst.brp_min = 1;
-	btconst.brp_max = 1024;
-	btconst.brp_inc = 1;
-	btconst.feature = GS_CAN_FEATURE_LISTEN_ONLY | GS_CAN_FEATURE_LOOP_BACK;
-
 	switch (req->bRequest) {
 
 		case GS_USB_BREQ_HOST_FORMAT:
@@ -266,12 +273,12 @@ static uint8_t USBD_GS_CAN_Vendor_Request(USBD_HandleTypeDef *pdev, USBD_SetupRe
 			break;
 
 		case GS_USB_BREQ_DEVICE_CONFIG:
-			memcpy(hcan->ep0_buf, &dconf, sizeof(dconf));
+			memcpy(hcan->ep0_buf, &USBD_GS_CAN_dconf, sizeof(USBD_GS_CAN_dconf));
 			USBD_CtlSendData(pdev, hcan->ep0_buf, req->wLength);
 			break;
 
 		case GS_USB_BREQ_BT_CONST:
-			memcpy(hcan->ep0_buf, &btconst, sizeof(btconst));
+			memcpy(hcan->ep0_buf, &USBD_GS_CAN_btconst, sizeof(USBD_GS_CAN_btconst));
 			USBD_CtlSendData(pdev, hcan->ep0_buf, req->wLength);
 			break;
 

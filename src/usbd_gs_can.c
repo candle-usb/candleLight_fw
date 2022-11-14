candle-usb/candleLight_fw

commit a0caf93e50edd081db3c6fbc3f36bc720c780a76

Remove flash-as-eeprom emulation for USER_ID
There is a pair of USB requests ( GS_USB_BREQ_GET_USER_ID and
_SET_USER_ID) that is not supported by the linux driver, and not used by
any known software :

https://github.com/candle-usb/candleLight_fw/issues/91

Since the firmware returns a unique serial #, there doesn't appear to be
any use case for assigning a user ID. Using a whole flash page for that
purpose can be wasteful on bigger targets .

This removes all the relevant code and adjusts the linker scripts
accordingly.

src/usbd_gs_can.c
@@ -40,7 +40,6 @@ THE SOFTWARE.
 #include "queue.h"
 #include "gpio.h"
 #include "timer.h"
-#include "flash.h"
 #include "util.h"
 
 typedef struct {
@@ -404,13 +403,6 @@ static uint8_t USBD_GS_CAN_EP0_RxReady(USBD_HandleTypeDef *pdev) {
 			}
 			break;
 
-		case GS_USB_BREQ_SET_USER_ID:
-			memcpy(&param_u32, hcan->ep0_buf, sizeof(param_u32));
-			if (flash_set_user_id(req->wValue, param_u32)) {
-				flash_flush();
-			}
-			break;
-
 		case GS_USB_BREQ_MODE:
 			if (req->wValue < NUM_CAN_CHANNEL) {
 
@@ -504,10 +496,6 @@ static uint8_t USBD_GS_CAN_Config_Request(USBD_HandleTypeDef *pdev, USBD_SetupRe
 		case GS_USB_BREQ_MODE:
 		case GS_USB_BREQ_BITTIMING:
 		case GS_USB_BREQ_IDENTIFY:
-		case GS_USB_BREQ_SET_USER_ID:
-			hcan->last_setup_request = *req;
-			USBD_CtlPrepareRx(pdev, hcan->ep0_buf, req->wLength);
-			break;
 
 		case GS_USB_BREQ_GET_TERMINATION:
 			term_state = get_term(req->wValue);
@@ -536,17 +524,6 @@ static uint8_t USBD_GS_CAN_Config_Request(USBD_HandleTypeDef *pdev, USBD_SetupRe
 			memcpy(hcan->ep0_buf, &hcan->sof_timestamp_us, sizeof(hcan->sof_timestamp_us));
 			USBD_CtlSendData(pdev, hcan->ep0_buf, sizeof(hcan->sof_timestamp_us));
 			break;
-
-		case GS_USB_BREQ_GET_USER_ID:
-			if (req->wValue < NUM_CAN_CHANNEL) {
-				d32 = flash_get_user_id(req->wValue);
-				memcpy(hcan->ep0_buf, &d32, sizeof(d32));
-				USBD_CtlSendData(pdev, hcan->ep0_buf, sizeof(d32));
-			} else {
-				USBD_CtlError(pdev, req);
-			}
-			break;
-
 
 		default:
 			USBD_CtlError(pdev, req);

CMakeLists.txt
@@ -68,7 +68,6 @@ set(
 
 	include/can.h src/can.c
 	include/dfu.h src/dfu.c
-	include/flash.h src/flash.c
 	include/gpio.h src/gpio.c
 	include/led.h src/led.c
 	include/queue.h src/queue.c

include/flash.h
@@ -1,35 +1,0 @@
-/*
-
-The MIT License (MIT)
-
-Copyright (c) 2016 Hubert Denkmair
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-*/
-
-#pragma once
-
-#include <stdbool.h>
-#include <stdint.h>
-
-void flash_load(void);
-bool flash_set_user_id(uint8_t channel, uint32_t user_id);
-uint32_t flash_get_user_id(uint8_t channel);
-void flash_flush(void);

include/gs_usb.h
@@ -173,8 +173,8 @@ enum gs_usb_breq {
 	GS_USB_BREQ_DEVICE_CONFIG,
 	GS_USB_BREQ_TIMESTAMP,
 	GS_USB_BREQ_IDENTIFY,
-	GS_USB_BREQ_GET_USER_ID,
-	GS_USB_BREQ_SET_USER_ID,
+	GS_USB_BREQ_GET_USER_ID,    //not implemented
+	GS_USB_BREQ_SET_USER_ID,    //not implemented
 	GS_USB_BREQ_DATA_BITTIMING,
 	GS_USB_BREQ_BT_CONST_EXT,
 	GS_USB_BREQ_SET_TERMINATION,

ldscripts/STM32F042X6.ld
@@ -2,13 +2,11 @@ __STACK_SIZE = 1024;
 __HEAP_SIZE  = 2560;
 
 __FLASH_SIZE = 32K;
-__NVM_SIZE = 1K;
 
 MEMORY
 {
-  FLASH (rx)  : ORIGIN = 0x08000000, LENGTH = __FLASH_SIZE - __NVM_SIZE
+  FLASH (rx)  : ORIGIN = 0x08000000, LENGTH = __FLASH_SIZE
   RAM   (rwx) : ORIGIN = 0x20000000, LENGTH = 6K
-  DATA (xrw) : ORIGIN = 0x08000000 + __FLASH_SIZE - __NVM_SIZE, LENGTH = __NVM_SIZE
 }
 
 ENTRY(Reset_Handler)
@@ -72,13 +70,6 @@ SECTIONS
   } > FLASH
 
   __etext = ALIGN (4);
-
-  .user_data :
-  {
-    . = ALIGN(4);
-    *(.user_data)
-    . = ALIGN(4);
-  } > DATA
 
   .data : AT (__etext)
   {

ldscripts/STM32F072XB.ld
@@ -2,13 +2,11 @@ __STACK_SIZE = 2K;
 __HEAP_SIZE  = 10K;
 
 __FLASH_SIZE = 128K;
-__NVM_SIZE = 1K;
 
 MEMORY
 {
-  FLASH (rx)  : ORIGIN = 0x08000000, LENGTH = __FLASH_SIZE - __NVM_SIZE
+  FLASH (rx)  : ORIGIN = 0x08000000, LENGTH = __FLASH_SIZE
   RAM   (rwx) : ORIGIN = 0x20000000, LENGTH = 16K
-  DATA (xrw) : ORIGIN = 0x08000000 + __FLASH_SIZE - __NVM_SIZE, LENGTH = __NVM_SIZE
 }
 
 
@@ -74,12 +72,6 @@ SECTIONS
 
   __etext = ALIGN (4);
 
-  .user_data :
-  {
-    . = ALIGN(4);
-    *(.user_data)
-    . = ALIGN(4);
-  } > DATA
 
   .data : AT (__etext)
   {

ldscripts/STM32F407XE.ld
@@ -2,13 +2,11 @@ __STACK_SIZE = 32K;
 __HEAP_SIZE  = 64K;
 
 __FLASH_SIZE = 512K;
-__NVM_SIZE = 128K;
 
 MEMORY
 {
-  FLASH (rx)  : ORIGIN = 0x08000000, LENGTH = __FLASH_SIZE - __NVM_SIZE
-  RAM   (rwx) : ORIGIN = 0x20000000, LENGTH = 128K
-  DATA (xrw) : ORIGIN = 0x08000000 + __FLASH_SIZE - __NVM_SIZE, LENGTH = __NVM_SIZE
+  FLASH (rx)  : ORIGIN = 0x08000000, LENGTH = __FLASH_SIZE
+  RAM   (rw) : ORIGIN = 0x20000000, LENGTH = 128K
 }
 
 
@@ -73,13 +71,6 @@ SECTIONS
   } > FLASH
 
   __etext = ALIGN (4);
-
-  .user_data :
-  {
-    . = ALIGN(4);
-    *(.user_data)
-    . = ALIGN(4);
-  } > DATA
 
   .data : AT (__etext)
   {

ldscripts/STM32G0B1XK.ld
@@ -2,13 +2,11 @@ __STACK_SIZE = 32K;
 __HEAP_SIZE  = 64K;
 
 __FLASH_SIZE = 512K;
-__NVM_SIZE = 144K;
 
 MEMORY
 {
-  FLASH (rx)  : ORIGIN = 0x08000000, LENGTH = __FLASH_SIZE - __NVM_SIZE
+  FLASH (rx)  : ORIGIN = 0x08000000, LENGTH = __FLASH_SIZE
   RAM   (rwx) : ORIGIN = 0x20000000, LENGTH = 144K
-  DATA (xrw) : ORIGIN = 0x08000000 + __FLASH_SIZE - __NVM_SIZE, LENGTH = __NVM_SIZE
 }
 
 ENTRY(Reset_Handler)
@@ -72,13 +70,6 @@ SECTIONS
   } > FLASH
 
   __etext = ALIGN (4);
-
-  .user_data :
-  {
-    . = ALIGN(4);
-    *(.user_data)
-    . = ALIGN(4);
-  } > DATA
 
   .data : AT (__etext)
   {

commit 7c88cfd571ee6d58bdf0056ff1f81de13f45edf8

usbd_gs_can: don't announce that we support GS_CAN_FEATURE_USER_ID
...it has been removed in a0caf93e50ed ("Remove flash-as-eeprom
emulation for USER_ID").

Fixes: a0caf93e50ed ("Remove flash-as-eeprom emulation for USER_ID")

src/usbd_gs_can.c
@@ -271,7 +271,6 @@ static const struct gs_device_bt_const USBD_GS_CAN_btconst = {
 	| GS_CAN_FEATURE_LOOP_BACK
 	| GS_CAN_FEATURE_HW_TIMESTAMP
 	| GS_CAN_FEATURE_IDENTIFY
-	| GS_CAN_FEATURE_USER_ID
 	| GS_CAN_FEATURE_PAD_PKTS_TO_MAX_PKT_SIZE
 #ifdef TERM_Pin
 	| GS_CAN_FEATURE_TERMINATION
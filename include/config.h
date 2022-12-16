694a520926b6ae5f0aeb496e9383b72e5cd104aa
Marc Kleine-Budde <mkl@pengutronix.de>
2022-12-16 16:01:05 +0100

    boards: add NUCLEO-G0B1RE

--- include/config.h
+++ include/config.h
@@ -361,6 +361,33 @@ THE SOFTWARE.
 	#define TERM_Mode				 GPIO_MODE_OUTPUT_PP
 	#define TERM_Active_High		 1
 
+#elif defined(BOARD_nucleo_g0b1re)
+	#define USBD_PRODUCT_STRING_FS	 (uint8_t *)"NUCLEO-G0B1RE gs_usb"
+	#define USBD_MANUFACTURER_STRING (uint8_t *)"STMicroelectronics"
+	#define DFU_INTERFACE_STRING_FS	 (uint8_t *)"NUCLEO-G0B1RE firmware upgrade interface"
+
+	#define TIM2_CLOCK_SPEED		 64000000
+
+	#define CAN_INTERFACE			 FDCAN1
+	#define CAN_INTERFACE2			 FDCAN2
+	#define CAN_CLOCK_SPEED			 64000000
+	#define NUM_CAN_CHANNEL			 1
+	#define CONFIG_CANFD			 1
+
+	#define LEDRX_GPIO_Port			 GPIOA
+	#define LEDRX_Pin				 GPIO_PIN_5
+	#define LEDRX_Mode				 GPIO_MODE_OUTPUT_PP
+	#define LEDRX_Active_High		 1
+
+	#define LEDTX_GPIO_Port			 GPIOA
+	#define LEDTX_Pin				 GPIO_PIN_5
+	#define LEDTX_Mode				 GPIO_MODE_OUTPUT_PP
+	#define LEDTX_Active_High		 1
+
+	#define USB_GPIO_Port			 GPIOA
+	#define USB_Pin_DM				 GPIO_PIN_11
+	#define USB_Pin_DP				 GPIO_PIN_12
+
 #elif defined(BOARD_STM32F4_DevBoard)
 	#define USBD_PRODUCT_STRING_FS	 (uint8_t*) "STM32F4VE Dev Board"
 	#define USBD_MANUFACTURER_STRING (uint8_t*) "misc"

candle-usb/candleLight_fw

commit f4f1586dbf563b4850d5b52e99aac06f0311403e

can.c: use GPIO_INIT_STATE to set CANSTBY pin

src/can.c
@@ -26,6 +26,7 @@
 
 #include "can.h"
 #include "config.h"
+#include "gpio.h"
 #include "gs_usb.h"
 #include "hal_include.h"
 
@@ -150,7 +151,7 @@ void can_enable(can_data_t *hcan, bool loop_back, bool listen_only, bool one_sho
 	can->FMR &= ~CAN_FMR_FINIT;
 
 #ifdef nCANSTBY_Pin
-	HAL_GPIO_WritePin(nCANSTBY_Port, nCANSTBY_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(nCANSTBY_Port, nCANSTBY_Pin, ~GPIO_INIT_STATE(nCANSTBY_Active_High));
 #endif
 }
 
@@ -158,7 +159,7 @@ void can_disable(can_data_t *hcan)
 {
 	CAN_TypeDef *can = hcan->instance;
 #ifdef nCANSTBY_Pin
-	HAL_GPIO_WritePin(nCANSTBY_Port, nCANSTBY_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(nCANSTBY_Port, nCANSTBY_Pin, GPIO_INIT_STATE(nCANSTBY_Active_High));
 #endif
 	can->MCR |= CAN_MCR_INRQ;     // send can controller into initialization mode
 }

commit 4a1e646cdb0b5b45bbc6c5bf77013205386f2da6

bxcan: can_enable(): fix disabling of standby pin
The binary inverse of 0x1 is 0xfe not 0x0. Fix this by using a logic
negation instead.

Fixes: f4f1586dbf56 ("can.c: use GPIO_INIT_STATE to set CANSTBY pin")

src/can.c
@@ -124,7 +124,7 @@ void can_enable(can_data_t *hcan, bool loop_back, bool listen_only, bool one_sho
 	can->FMR &= ~CAN_FMR_FINIT;
 
 #ifdef nCANSTBY_Pin
-	HAL_GPIO_WritePin(nCANSTBY_Port, nCANSTBY_Pin, ~GPIO_INIT_STATE(nCANSTBY_Active_High));
+	HAL_GPIO_WritePin(nCANSTBY_Port, nCANSTBY_Pin, !GPIO_INIT_STATE(nCANSTBY_Active_High));
 #endif
 }
 
7955efd060e73adef27a5009c5670533fa5095e8
Marc Kleine-Budde <mkl@pengutronix.de>
2023-01-16 16:30:01 +0100

    budgetcan: only 1 channel for now

--- include/config.h
+++ include/config.h
@@ -335,31 +335,31 @@ THE SOFTWARE.
 	#define CAN_INTERFACE			 FDCAN1
 	#define CAN_INTERFACE2			 FDCAN2
 	#define CAN_CLOCK_SPEED			 64000000
-	#define NUM_CAN_CHANNEL			 2
-	#define CONFIG_CANFD
+	#define NUM_CAN_CHANNEL			 1
+	#define CONFIG_CANFD			 1
 
-	#define nCANSTBY_Port		 GPIOA
-	#define nCANSTBY_Pin		 GPIO_PIN_0    /* control xceiver standby, active low */
-	#define nCANSTBY_Active_High 0
+	#define nCANSTBY_Port			 GPIOA
+	#define nCANSTBY_Pin			 GPIO_PIN_0 /* control xceiver standby, active low */
+	#define nCANSTBY_Active_High	 0
 
-	#define LEDRX_GPIO_Port		 GPIOB
-	#define LEDRX_Pin			 GPIO_PIN_4
-	#define LEDRX_Mode			 GPIO_MODE_OUTPUT_PP
-	#define LEDRX_Active_High	 1
+	#define LEDRX_GPIO_Port			 GPIOB
+	#define LEDRX_Pin				 GPIO_PIN_4
+	#define LEDRX_Mode				 GPIO_MODE_OUTPUT_PP
+	#define LEDRX_Active_High		 1
 
-	#define LEDTX_GPIO_Port		 GPIOB
-	#define LEDTX_Pin			 GPIO_PIN_3
-	#define LEDTX_Mode			 GPIO_MODE_OUTPUT_PP
-	#define LEDTX_Active_High	 1
+	#define LEDTX_GPIO_Port			 GPIOB
+	#define LEDTX_Pin				 GPIO_PIN_3
+	#define LEDTX_Mode				 GPIO_MODE_OUTPUT_PP
+	#define LEDTX_Active_High		 1
 
-	#define USB_GPIO_Port		 GPIOA
-	#define USB_Pin_DM			 GPIO_PIN_11
-	#define USB_Pin_DP			 GPIO_PIN_12
+	#define USB_GPIO_Port			 GPIOA
+	#define USB_Pin_DM				 GPIO_PIN_11
+	#define USB_Pin_DP				 GPIO_PIN_12
 
-	#define TERM_GPIO_Port		 GPIOA
-	#define TERM_Pin			 GPIO_PIN_1
-	#define TERM_Mode			 GPIO_MODE_OUTPUT_PP
-	#define TERM_Active_High	 1
+	#define TERM_GPIO_Port			 GPIOA
+	#define TERM_Pin				 GPIO_PIN_1
+	#define TERM_Mode				 GPIO_MODE_OUTPUT_PP
+	#define TERM_Active_High		 1
 
 #elif defined(BOARD_STM32F4_DevBoard)
 	#define USBD_PRODUCT_STRING_FS	 (uint8_t*) "STM32F4VE Dev Board"

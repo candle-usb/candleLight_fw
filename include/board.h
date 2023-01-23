03c6f0d15f39661b7334787555764ad1102740a7
Jonas Martin <j.martin@pengutronix.de>
2023-01-23 09:16:01 +0100

    board: legacy: introduce legacy boards and move CAN_INTERFACE to it

---
+++ include/board.h
@@ -0,0 +1,44 @@
+#pragma once
+
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2023 Pengutronix,
+              Jonas Martin <kernel@pengutronix.de>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+#include "usbd_gs_can.h"
+
+struct BoardChannelConfig {
+#if defined(STM32G0)
+	FDCAN_GlobalTypeDef *interface;
+#else
+	CAN_TypeDef *interface;
+#endif
+};
+
+struct BoardConfig {
+	struct BoardChannelConfig channels[NUM_CAN_CHANNEL];
+};
+
+extern const struct BoardConfig config;

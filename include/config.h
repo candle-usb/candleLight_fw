07a618c406666436119cc061f6870ab7b5b43056
Jonas Martin <j.martin@pengutronix.de>
2023-06-20 11:35:44 +0200

    boards: add CandleLightFD

--- include/config.h
+++ include/config.h
@@ -346,6 +346,17 @@ THE SOFTWARE.
 	#define NUM_CAN_CHANNEL			 2
 	#define CONFIG_CANFD			 1
 
+#elif defined(BOARD_candleLightFD)
+	#define USBD_PRODUCT_STRING_FS	 (uint8_t*) "candleLightFD gs_usb"
+	#define USBD_MANUFACTURER_STRING (uint8_t*) "candleLightFD"
+	#define DFU_INTERFACE_STRING_FS	 (uint8_t*) "candleLightFD firmware upgrade interface"
+
+	#define TIM2_CLOCK_SPEED		 64000000
+
+	#define CAN_CLOCK_SPEED			 40000000
+	#define NUM_CAN_CHANNEL			 1
+	#define CONFIG_CANFD			 1
+
 #elif defined(BOARD_STM32F4_DevBoard)
 	#define USBD_PRODUCT_STRING_FS	 (uint8_t*) "STM32F4VE Dev Board"
 	#define USBD_MANUFACTURER_STRING (uint8_t*) "misc"

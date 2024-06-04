51f29f14c8d44b51266ff116787efe95c01ca378
Marc Kleine-Budde <mkl@blackshift.org>
2024-06-04 22:48:44 +0200

    stm32g0b1: remove unused USB_GPIO_Port

--- include/config.h
+++ include/config.h
@@ -136,10 +136,6 @@ THE SOFTWARE.
 	#define LEDTX_Mode				 GPIO_MODE_OUTPUT_PP
 	#define LEDTX_Active_High		 0
 
-	#define USB_GPIO_Port			 GPIOA
-	#define USB_Pin_DM				 GPIO_PIN_11
-	#define USB_Pin_DP				 GPIO_PIN_12
-
 #elif defined(BOARD_DSD_TECH_SH_C30A)
 	#define USBD_PRODUCT_STRING_FS	 "SH-C30A USB to CAN adapter"
 	#define USBD_MANUFACTURER_STRING "DSD TECH"
@@ -323,10 +319,6 @@ THE SOFTWARE.
 	#define LEDTX_Pin				 GPIO_PIN_3
 	#define LEDTX_Mode				 GPIO_MODE_OUTPUT_PP
 	#define LEDTX_Active_High		 1
-
-	#define USB_GPIO_Port			 GPIOA
-	#define USB_Pin_DM				 GPIO_PIN_11
-	#define USB_Pin_DP				 GPIO_PIN_12
 
 	#define TERM_GPIO_Port			 GPIOA
 	#define TERM_Pin				 GPIO_PIN_1

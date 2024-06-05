72fe9e0c2e0a5970acc5a66e8ffdfa703537d706
Marc Kleine-Budde <mkl@blackshift.org>
2024-06-05 14:18:22 +0200

    cmake: remove whitespace after '(' and before ')'

--- cmake/FindDFUSuffix.cmake
+++ cmake/FindDFUSuffix.cmake
@@ -1,8 +1,8 @@
 # find and set DFU_SUFFIX_EXECUTABLE
 
-find_program (DFU_SUFFIX_EXECUTABLE
+find_program(DFU_SUFFIX_EXECUTABLE
 	NAMES dfu-suffix
 	DOC "dfu-suffix executable"
 )
 
-mark_as_advanced (DFU_SUFFIX_EXECUTABLE)
+mark_as_advanced(DFU_SUFFIX_EXECUTABLE)

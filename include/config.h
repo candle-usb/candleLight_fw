38a9a19284491ecd8a69e08435e39896b501a899
Romain Reignier <romain@reignier.sh>
2024-10-09 14:58:52 +0200

    boards: add WeActStudio USB2CANFDV1

--- include/config.h
+++ include/config.h
@@ -350,6 +350,18 @@ THE SOFTWARE.
 	#define NUM_CAN_CHANNEL			 1
 	#define CONFIG_CANFD			 1
 
+#elif defined(BOARD_WeActStudio_USB2CANFDV1)
+	#define USBD_PRODUCT_STRING_FS	 (uint8_t*) "USB2CANFDV1 gs_usb"
+	#define USBD_MANUFACTURER_STRING (uint8_t*) "WeActStudio"
+	#define DFU_INTERFACE_STRING_FS	 (uint8_t*) "USB2CANFDV1 firmware upgrade interface"
+
+	#define HSE_OSC_SPEED			 16000000
+	#define TIM2_CLOCK_SPEED		 64000000
+
+	#define CAN_CLOCK_SPEED			 40000000
+	#define NUM_CAN_CHANNEL			 1
+	#define CONFIG_CANFD			 1
+
 #elif defined(BOARD_budgetcan)
 	#define USBD_PRODUCT_STRING_FS	 "budgetcan gs_usb"
 	#define USBD_MANUFACTURER_STRING "budgetcan"

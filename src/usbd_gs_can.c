candle-usb/candleLight_fw

commit 7fba5612ae268975e0456fcb22350bab6b0a951c

usbd_gs_can: USBD_GS_CAN_ReceiveFromHost(): call USBD_GS_CAN_PrepareReceive() with IRQs enabled

src/usbd_gs_can.c
@@ -728,8 +728,9 @@ void USBD_GS_CAN_ReceiveFromHost(USBD_HandleTypeDef *pdev)
 	}
 
 	list_del(&hcan->from_host_buf->list);
-	USBD_GS_CAN_PrepareReceive(pdev);
 	restore_irq(was_irq_enabled);
+
+	USBD_GS_CAN_PrepareReceive(pdev);
 }
 
 static uint8_t USBD_GS_CAN_Transmit(USBD_HandleTypeDef *pdev, uint8_t *buf, uint16_t len)

commit d97f265e71d5e517ad7cdf37b4c6dca054281494

gs_usb: USBD_GS_CAN_ReceiveFromHost(): ensure RX endpoint buffer is prepared with interrupts disabled.
This avoids writing the received USB frames from the host in a wrong msgbuf.

Fixes: 7fba5612ae26 ("usbd_gs_can: USBD_GS_CAN_ReceiveFromHost(): call USBD_GS_CAN_PrepareReceive() with IRQs enabled")

src/usbd_gs_can.c
@@ -774,9 +774,9 @@ void USBD_GS_CAN_ReceiveFromHost(USBD_HandleTypeDef *pdev)
 	}
 
 	list_del(&hcan->from_host_buf->list);
-	restore_irq(was_irq_enabled);
 
 	USBD_GS_CAN_PrepareReceive(pdev);
+	restore_irq(was_irq_enabled);
 }
 
 static uint8_t USBD_GS_CAN_Transmit(USBD_HandleTypeDef *pdev, uint8_t *buf, uint16_t len)
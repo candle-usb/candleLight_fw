candle-usb/candleLight_fw

commit f418aa362c5c24e4a4fc0eb2a6475be0a5a1dfbd

usbd_gs_can: clean up send to host path
The USBD_GS_CAN_SendToHost() function is used to send to struct
gs_host_frame_object to the host.

Until this patch, after the sending process has been started, the
outgoing frame object is already added to the list of free objects and
the variable USBD_GS_CAN_HandleTypeDef::TxState is used to track if
the transfer to the host is in progress.

Instead, hold the outgoing object in
USBD_GS_CAN_HandleTypeDef::to_host_buf and move it to the free list
after the transfer is finished in USBD_GS_CAN_DataIn(). Use this to
track if a transfer is ongoing.

src/usbd_gs_can.c
@@ -567,10 +567,14 @@ static uint8_t USBD_GS_CAN_EP0_RxReady(USBD_HandleTypeDef *pdev) {
 }
 
 static uint8_t USBD_GS_CAN_DataIn(USBD_HandleTypeDef *pdev, uint8_t epnum) {
+	USBD_GS_CAN_HandleTypeDef *hcan = (USBD_GS_CAN_HandleTypeDef*)pdev->pClassData;
 	(void) epnum;
 
-	USBD_GS_CAN_HandleTypeDef *hcan = (USBD_GS_CAN_HandleTypeDef*)pdev->pClassData;
-	hcan->TxState = 0;
+	bool was_irq_enabled = disable_irq();
+	list_add_tail(&hcan->to_host_buf->list, &hcan->list_frame_pool);
+	hcan->to_host_buf = NULL;
+	restore_irq(was_irq_enabled);
+
 	return USBD_OK;
 }
 
@@ -705,7 +709,7 @@ bool USBD_GS_CAN_CustomInterfaceRequest(USBD_HandleTypeDef *pdev, USBD_SetupReqT
 	return USBD_GS_CAN_CustomDeviceRequest(pdev, req);
 }
 
-bool USBD_GS_CAN_TxReady(USBD_HandleTypeDef *pdev)
+void USBD_GS_CAN_ReceiveFromHost(USBD_HandleTypeDef *pdev)
 {
 	USBD_GS_CAN_HandleTypeDef *hcan = (USBD_GS_CAN_HandleTypeDef*)pdev->pClassData;
 	bool was_irq_enabled = disable_irq();
@@ -718,16 +722,13 @@ bool USBD_GS_CAN_TxReady(USBD_HandleTypeDef *pdev)
 			USBD_GS_CAN_PrepareReceive(pdev);
 		}
 	}
-	bool result = hcan->TxState == 0;
+
 	restore_irq(was_irq_enabled);
-	return result;
 }
 
 static uint8_t USBD_GS_CAN_Transmit(USBD_HandleTypeDef *pdev, uint8_t *buf, uint16_t len)
 {
-	USBD_GS_CAN_HandleTypeDef *hcan = (USBD_GS_CAN_HandleTypeDef*)pdev->pClassData;
-	if (hcan->TxState == 0 && (false == is_usb_suspend_cb)) {
-		hcan->TxState = 1;
+	if (false == is_usb_suspend_cb) {
 		USBD_LL_Transmit(pdev, GSUSB_ENDPOINT_IN, buf, len);
 		return USBD_OK;
 	} else {
@@ -770,24 +771,32 @@ static uint8_t USBD_GS_CAN_SendFrame(USBD_HandleTypeDef *pdev, struct gs_host_fr
 void USBD_GS_CAN_SendToHost(USBD_HandleTypeDef *pdev)
 {
 	USBD_GS_CAN_HandleTypeDef *hcan = (USBD_GS_CAN_HandleTypeDef*)pdev->pClassData;
-	struct gs_host_frame_object *frame_object;
 
 	bool was_irq_enabled = disable_irq();
-	frame_object = list_first_entry_or_null(&hcan->list_to_host,
-											struct gs_host_frame_object,
-											list);
-	if (!frame_object) {
+	if (hcan->to_host_buf) {
 		restore_irq(was_irq_enabled);
 		return;
 	}
-	list_del(&frame_object->list);
+
+	hcan->to_host_buf = list_first_entry_or_null(&hcan->list_to_host,
+												 struct gs_host_frame_object,
+												 list);
+	if (!hcan->to_host_buf) {
+		restore_irq(was_irq_enabled);
+		return;
+	}
+
+	list_del(&hcan->to_host_buf->list);
 	restore_irq(was_irq_enabled);
 
-	if (USBD_GS_CAN_SendFrame(pdev, &frame_object->frame) == USBD_OK) {
-		list_add_tail_locked(&frame_object->list, &hcan->list_frame_pool);
-	} else {
-		list_add_locked(&frame_object->list, &hcan->list_to_host);
-	}
+	uint8_t result = USBD_GS_CAN_SendFrame(pdev, &hcan->to_host_buf->frame);
+	if (result == USBD_OK)
+		return;
+
+	was_irq_enabled = disable_irq();
+	list_add(&hcan->to_host_buf->list, &hcan->list_frame_pool);
+	hcan->to_host_buf = NULL;
+	restore_irq(was_irq_enabled);
 }
 
 bool USBD_GS_CAN_DfuDetachRequested(USBD_HandleTypeDef *pdev)
@@ -814,7 +823,6 @@ void USBD_GS_CAN_SuspendCallback(USBD_HandleTypeDef  *pdev)
 
 void USBD_GS_CAN_ResumeCallback(USBD_HandleTypeDef  *pdev)
 {
-	USBD_GS_CAN_HandleTypeDef *hcan = (USBD_GS_CAN_HandleTypeDef*) pdev->pClassData;
-	hcan->TxState = 0;
+	(void)pdev;
 	is_usb_suspend_cb = false;
 }

include/usbd_gs_can.h
@@ -61,14 +61,13 @@ struct gs_host_frame_object {
 typedef struct {
 	uint8_t __aligned(4) ep0_buf[GS_CAN_EP0_BUF_SIZE];
 
-	__IO uint32_t TxState;
-
 	USBD_SetupReqTypedef last_setup_request;
 
 	struct list_head list_frame_pool;
 	struct list_head list_to_host;
 
 	struct gs_host_frame_object *from_host_buf;
+	struct gs_host_frame_object *to_host_buf;
 
 	can_data_t channels[NUM_CAN_CHANNEL];
 
@@ -98,7 +97,7 @@ typedef struct {
 uint8_t USBD_GS_CAN_Init(USBD_GS_CAN_HandleTypeDef *hcan, USBD_HandleTypeDef *pdev);
 void USBD_GS_CAN_SuspendCallback(USBD_HandleTypeDef  *pdev);
 void USBD_GS_CAN_ResumeCallback(USBD_HandleTypeDef  *pdev);
-bool USBD_GS_CAN_TxReady(USBD_HandleTypeDef *pdev);
+void USBD_GS_CAN_ReceiveFromHost(USBD_HandleTypeDef *pdev);
 void USBD_GS_CAN_SendToHost(USBD_HandleTypeDef *pdev);
 bool USBD_GS_CAN_CustomDeviceRequest(USBD_HandleTypeDef *pdev, USBD_SetupReqTypedef *req);
 bool USBD_GS_CAN_CustomInterfaceRequest(USBD_HandleTypeDef *pdev, USBD_SetupReqTypedef *req);

src/main.c
@@ -126,9 +126,8 @@ int main(void)
 			restore_irq(was_irq_enabled);
 		}
 
-		if (USBD_GS_CAN_TxReady(&hUSB)) {
-			USBD_GS_CAN_SendToHost(&hUSB);
-		}
+		USBD_GS_CAN_ReceiveFromHost(&hUSB);
+		USBD_GS_CAN_SendToHost(&hUSB);
 
 		if (can_is_rx_pending(channel)) {
 			bool was_irq_enabled = disable_irq();

commit 0080001f288439cfb96fbae8fe4832767f5234a8

usbd_gs_can: USBD_GS_CAN_SendToHost(): return untransmitted message back to transmit queue, rather than free queue
Fixes: f418aa362c5c ("usbd_gs_can: clean up send to host path")

src/usbd_gs_can.c
@@ -897,7 +897,7 @@ void USBD_GS_CAN_SendToHost(USBD_HandleTypeDef *pdev)
 		return;
 
 	was_irq_enabled = disable_irq();
-	list_add(&hcan->to_host_buf->list, &hcan->list_frame_pool);
+	list_add(&hcan->to_host_buf->list, &hcan->list_to_host);
 	hcan->to_host_buf = NULL;
 	restore_irq(was_irq_enabled);
 }
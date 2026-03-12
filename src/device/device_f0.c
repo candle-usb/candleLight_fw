7bed2703e4f06a0057ad5d25a52db628e05a9921
Marc Kleine-Budde <mkl@blackshift.org>
2026-03-12 14:58:09 +0100

    stm32f072: device_f0: cleanup coding style + convert to C99 initializers

--- src/device/device_f0.c
+++ src/device/device_f0.c
@@ -1,30 +1,26 @@
 /*
-
-The MIT License (MIT)
-
-Copyright (c) 2016 Hubert Denkmair
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-*/
-/*
- * F0 device-specific code
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Hubert Denkmair
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  */
 
 #include "board.h"
@@ -32,63 +28,63 @@ THE SOFTWARE.
 #include "device.h"
 #include "hal_include.h"
 
-void device_can_init(can_data_t *channel, const struct board_channel_config *channel_config) {
+void device_can_init(can_data_t *channel, const struct board_channel_config *channel_config)
+{
 	__HAL_RCC_CAN1_CLK_ENABLE();
 
-	GPIO_InitTypeDef itd;
-
-	itd.Pin = GPIO_PIN_8|GPIO_PIN_9;
-	itd.Mode = GPIO_MODE_AF_PP;
-	itd.Pull = GPIO_NOPULL;
-	itd.Speed = GPIO_SPEED_FREQ_HIGH;
-	itd.Alternate = GPIO_AF4_CAN;
+	GPIO_InitTypeDef itd = {
+		.Pin = GPIO_PIN_8 | GPIO_PIN_9,
+		.Mode = GPIO_MODE_AF_PP,
+		.Pull = GPIO_NOPULL,
+		.Speed = GPIO_SPEED_FREQ_HIGH,
+		.Alternate = GPIO_AF4_CAN,
+	};
 	HAL_GPIO_Init(GPIOB, &itd);
 
-	channel->instance   = channel_config->interface;
-	channel->brp        = 6;
-	channel->sjw        = 1;
+	channel->instance = channel_config->interface;
+	channel->brp = 6;
+	channel->sjw = 1;
 	channel->phase_seg1 = 13;
 	channel->phase_seg2 = 2;
-	return;
 }
 
 void device_sysclock_config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct;
-	RCC_ClkInitTypeDef RCC_ClkInitStruct;
-
-	RCC_PeriphCLKInitTypeDef PeriphClkInit;
-	RCC_CRSInitTypeDef RCC_CRSInitStruct;
-
 	__HAL_RCC_SYSCFG_CLK_ENABLE();
 	__HAL_RCC_PWR_CLK_ENABLE();
 
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
-	RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	const RCC_OscInitTypeDef RCC_OscInitStruct = {
+		.OscillatorType = RCC_OSCILLATORTYPE_HSI48,
+		.HSI48State = RCC_HSI48_ON,
+		.PLL.PLLState = RCC_PLL_NONE,
+	};
 	HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK |
-								  RCC_CLOCKTYPE_SYSCLK |
-								  RCC_CLOCKTYPE_PCLK1;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	const RCC_ClkInitTypeDef RCC_ClkInitStruct = {
+		.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1,
+		.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48,
+		.AHBCLKDivider = RCC_SYSCLK_DIV1,
+		.APB1CLKDivider = RCC_HCLK_DIV1,
+	};
 	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1);
 
-	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
-	PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = {
+		.PeriphClockSelection = RCC_PERIPHCLK_USB,
+		.UsbClockSelection = RCC_USBCLKSOURCE_HSI48,
+	};
 	HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
 
 	__HAL_RCC_CRS_CLK_ENABLE();
-	RCC_CRSInitStruct.Prescaler = RCC_CRS_SYNC_DIV1;
-	RCC_CRSInitStruct.Source = RCC_CRS_SYNC_SOURCE_USB;
-	RCC_CRSInitStruct.Polarity = RCC_CRS_SYNC_POLARITY_RISING;
-	RCC_CRSInitStruct.ReloadValue = __HAL_RCC_CRS_RELOADVALUE_CALCULATE(48000000,1000);
-	RCC_CRSInitStruct.ErrorLimitValue = 34;
-	RCC_CRSInitStruct.HSI48CalibrationValue = 32;
+	RCC_CRSInitTypeDef RCC_CRSInitStruct = {
+		.Prescaler = RCC_CRS_SYNC_DIV1,
+		.Source = RCC_CRS_SYNC_SOURCE_USB,
+		.Polarity = RCC_CRS_SYNC_POLARITY_RISING,
+		.ReloadValue = __HAL_RCC_CRS_RELOADVALUE_CALCULATE(48000000, 1000),
+		.ErrorLimitValue = 34,
+		.HSI48CalibrationValue = 32,
+	};
 	HAL_RCCEx_CRSConfig(&RCC_CRSInitStruct);
 
-	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
 
 	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 

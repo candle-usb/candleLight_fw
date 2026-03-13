7c579bda59626d6f8157fc19459645d7bc617889
Marc Kleine-Budde <mkl@blackshift.org>
2026-03-13 23:54:39 +0100

    stm32f4: device_f4: cleanup coding style + convert to C99 initializers

--- src/device/device_f4.c
+++ src/device/device_f4.c
@@ -1,59 +1,54 @@
 /*
-
-The MIT License (MIT)
-
-Copyright (c) 2022 Ricky Lopez
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-*/
-/* F4-specific code */
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Ricky Lopez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
 
 #include "board.h"
 #include "can.h"
 #include "device.h"
 #include "hal_include.h"
 
-void device_can_init(can_data_t *channel, const struct board_channel_config *channel_config) {
+void device_can_init(can_data_t *channel, const struct board_channel_config *channel_config)
+{
 	__HAL_RCC_CAN1_CLK_ENABLE();
 
-	GPIO_InitTypeDef itd;
+	GPIO_InitTypeDef itd_can1 = {
+		.Pin = GPIO_PIN_0 | GPIO_PIN_1,
+		.Mode = GPIO_MODE_AF_PP,
+		.Pull = GPIO_NOPULL,
+		.Speed = GPIO_SPEED_FREQ_VERY_HIGH,
+		.Alternate = GPIO_AF9_CAN1,
+	};
+	HAL_GPIO_Init(GPIOD, &itd_can1);
 
-	itd.Pin = GPIO_PIN_0|GPIO_PIN_1;
-	itd.Mode = GPIO_MODE_AF_PP;
-	itd.Pull = GPIO_NOPULL;
-	itd.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-	itd.Alternate = GPIO_AF9_CAN1;
-	HAL_GPIO_Init(GPIOD, &itd);
-
-	channel->instance   = channel_config->interface;
-	channel->brp        = 6;
-	channel->sjw        = 1;
+	channel->instance = channel_config->interface;
+	channel->brp = 6;
+	channel->sjw = 1;
 	channel->phase_seg1 = 12;
 	channel->phase_seg2 = 1;
-	return;
 }
 
 void device_sysclock_config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct;
-	RCC_ClkInitTypeDef RCC_ClkInitStruct;
-
 	__HAL_RCC_SYSCFG_CLK_ENABLE();
 	__HAL_RCC_PWR_CLK_ENABLE();
 
@@ -64,27 +59,29 @@ void device_sysclock_config(void) {
 	 * Initializes the RCC Oscillators according to the specified
 	 * parameters in the RCC_OscInitTypeDef structure.
 	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLM = 4;
-	RCC_OscInitStruct.PLL.PLLN = 168;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-	RCC_OscInitStruct.PLL.PLLQ = 7;
+	RCC_OscInitTypeDef RCC_OscInitStruct = {
+		.OscillatorType = RCC_OSCILLATORTYPE_HSE,
+		.HSEState = RCC_HSE_ON,
+		.PLL.PLLState = RCC_PLL_ON,
+		.PLL.PLLSource = RCC_PLLSOURCE_HSE,
+		.PLL.PLLM = 4,
+		.PLL.PLLN = 168,
+		.PLL.PLLP = RCC_PLLP_DIV2,
+		.PLL.PLLQ = 7,
+	};
 	HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK |
-								  RCC_CLOCKTYPE_SYSCLK |
-								  RCC_CLOCKTYPE_PCLK1 |
-								  RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {
+		.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+					 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2,
+		.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK,
+		.AHBCLKDivider = RCC_SYSCLK_DIV1,
+		.APB1CLKDivider = RCC_HCLK_DIV4,
+		.APB2CLKDivider = RCC_HCLK_DIV2,
+	};
 	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
 
-	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
 
 	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 

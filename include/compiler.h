8eb597913570be9fd81c6e1dc2b65a61c368472a
Marc Kleine-Budde <mkl@blackshift.org>
2026-03-14 00:07:01 +0100

    compiler.h: fix the definitions of max5(), max6()
    Fixes: 35fb74228b4c ("compiler.h: add some min() and max() macros")
    Fixes: 68cd25d5fd17 ("usb: add support for CAN HW-filter")

--- include/compiler.h
+++ include/compiler.h
@@ -107,9 +107,9 @@
 #define max4(a, b, c, d)	   max(max(a, b), max(b, d))
 
 #define min5(a, b, c, d, e)	   min3(min(a, b), min(c, d), e)
-#define max5(a, b, c, d, e)	   max3(max(a, b), max(b, d), e)
+#define max5(a, b, c, d, e)	   max3(max(a, b), max(c, d), e)
 
 #define min6(a, b, c, d, e, f) min3(min(a, b), min(c, d), min(e, f))
-#define max6(a, b, c, d, e, f) max3(max(a, b), max(b, d), max(e, f))
+#define max6(a, b, c, d, e, f) max3(max(a, b), max(c, d), max(e, f))
 
 #endif /* _LINUXKPI_LINUX_COMPILER_H_ */

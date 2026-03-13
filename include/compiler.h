candle-usb/candleLight_fw

commit 35fb74228b4c1cf2bd2ed8ce17edccdf53b07a29

compiler.h: add some min() and max() macros

include/compiler.h
@@ -93,4 +93,16 @@
 		_t _n[0]; \
 	}
 
+#define min(x, y)			((x) < (y) ? (x) : (y))
+#define max(x, y)			((x) > (y) ? (x) : (y))
+
+#define min3(a, b, c)		min(a, min(b, c))
+#define max3(a, b, c)		max(a, max(b, c))
+
+#define min4(a, b, c, d)	min(min(a, b), min(c, d))
+#define max4(a, b, c, d)	max(max(a, b), max(b, d))
+
+#define min5(a, b, c, d, e) min3(min(a, b), min(c, d), e)
+#define max5(a, b, c, d, e) max3(max(a, b), max(b, d), e)
+
 #endif /* _LINUXKPI_LINUX_COMPILER_H_ */

commit 8eb597913570be9fd81c6e1dc2b65a61c368472a

compiler.h: fix the definitions of max5(), max6()
Fixes: 35fb74228b4c ("compiler.h: add some min() and max() macros")
Fixes: 68cd25d5fd17 ("usb: add support for CAN HW-filter")

include/compiler.h
@@ -107,9 +107,9 @@
 #define max4(a, b, c, d)	   max(max(a, b), max(b, d))
 
 #define min5(a, b, c, d, e)	   min3(min(a, b), min(c, d), e)
-#define max5(a, b, c, d, e)	   max3(max(a, b), max(b, d), e)
+#define max5(a, b, c, d, e)	   max3(max(a, b), max(c, d), e)
 
 #define min6(a, b, c, d, e, f) min3(min(a, b), min(c, d), min(e, f))
-#define max6(a, b, c, d, e, f) max3(max(a, b), max(b, d), max(e, f))
+#define max6(a, b, c, d, e, f) max3(max(a, b), max(c, d), max(e, f))
 
 #endif /* _LINUXKPI_LINUX_COMPILER_H_ */
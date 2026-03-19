candle-usb/candleLight_fw

commit 35fb74228b4c1cf2bd2ed8ce17edccdf53b07a29

compiler.h: add some min() and max() macros

include/compiler.h
@@ -93,4 +93,16 @@
 		_t _n[0]; \
 	}
 
+#define min(x, y)			((x) < (y) ? (x) : (y))
+#define max(x, y)			((x) > (y) ? (x) : (y))
+
+#define min3(a, b, c)		min(a, min(b, c))
+#define max3(a, b, c)		max(a, max(b, c))
+
+#define min4(a, b, c, d)	min(min(a, b), min(c, d))
+#define max4(a, b, c, d)	max(max(a, b), max(b, d))
+
+#define min5(a, b, c, d, e) min3(min(a, b), min(c, d), e)
+#define max5(a, b, c, d, e) max3(max(a, b), max(b, d), e)
+
 #endif /* _LINUXKPI_LINUX_COMPILER_H_ */

commit 911e31dafa128ef08af4a585ba7dd3b5c1312408

compiler.h: fix the definitions of max4()
Fixes: 35fb74228b4c ("compiler.h: add some min() and max() macros")

include/compiler.h
@@ -104,7 +104,7 @@
 #define max3(a, b, c)		   max(a, max(b, c))
 
 #define min4(a, b, c, d)	   min(min(a, b), min(c, d))
-#define max4(a, b, c, d)	   max(max(a, b), max(b, d))
+#define max4(a, b, c, d)	   max(max(a, b), max(c, d))
 
 #define min5(a, b, c, d, e)	   min3(min(a, b), min(c, d), e)
 #define max5(a, b, c, d, e)	   max3(max(a, b), max(c, d), e)
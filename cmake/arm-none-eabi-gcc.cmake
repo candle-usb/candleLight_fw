6942ae2d5687141bdc82b63afd0f39c82e5833b8
Marc Kleine-Budde <mkl@blackshift.org>
2026-05-18 11:42:18 +0200

    cmake: add search hints for toolchain

--- cmake/arm-none-eabi-gcc.cmake
+++ cmake/arm-none-eabi-gcc.cmake
@@ -1,7 +1,20 @@
+set(TOOLCHAIN arm-none-eabi_14.2.rel1-1)
+
+find_path(TOOLCHAIN_BIN_DIR
+	NAMES arm-none-eabi-gcc
+	HINTS
+		ENV TOOLCHAIN_BIN_DIR
+		"$ENV{HOME}/${TOOLCHAIN}/bin"
+		"/opt/${TOOLCHAIN}/bin"
+		"/srv/${TOOLCHAIN}/bin"
+		"/usr/local/${TOOLCHAIN}/bin"
+	DOC "Path to the ARM toolchain binaries"
+)
+
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_C_COMPILER "${TOOLCHAIN_BIN_DIR}/arm-none-eabi-gcc")
 set(CMAKE_C_COMPILER_TARGET arm-none-eabi)
 
 SET (CMAKE_C_COMPILER_WORKS 1)

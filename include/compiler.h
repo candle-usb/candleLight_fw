candle-usb/candleLight_fw

commit 4cc0cbcc9f888c8dd7c1949c53b0d38cca94b9dd

no-op: move no-op from main.c here + clean up coding style

src/no-op.c
@@ -1,0 +1,40 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2024 Marc Kleine-Budde <kernel@pengutronix.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+void _close(void)
+{
+}
+
+void _lseek(void)
+{
+}
+
+void _read(void)
+{
+}
+
+void _write(void)
+{
+}

CMakeLists.txt
@@ -131,6 +131,7 @@ set(
 
 	src/interrupts.c
 	src/main.c
+	src/no-op.c
 	src/startup.c
 
 	${CMAKE_CURRENT_BINARY_DIR}/version.h

src/main.c
@@ -49,15 +49,6 @@ THE SOFTWARE.
 static USBD_GS_CAN_HandleTypeDef hGS_CAN;
 static USBD_HandleTypeDef hUSB = {0};
 
-void __weak _close(void) {
-}
-void __weak _lseek(void) {
-}
-void __weak _read(void) {
-}
-void __weak _write(void) {
-}
-
 int main(void)
 {
 	HAL_Init();

commit f12bb9dad13d6338ef9f1401d58068c3f9d53760

no-op: add weak attribute funktions implementing libc no-op functions
This is a preparation patch for semihosting support.

With activated semihosting the FW is against rdimon and libgloss, which
fails due to multiple definitions of the same symbol. Re-add the __weak
attribute which was lost in commit 4cc0cbcc9f88 ("no-op: move no-op from
main.c here + clean up coding style").

Fixes: 4cc0cbcc9f88 ("no-op: move no-op from main.c here + clean up coding style")

include/compiler.h
@@ -52,6 +52,10 @@
 #define fallthrough do {} while (0) /* fallthrough */
 #endif
 
+#ifndef __weak
+#define __weak  __attribute__((weak))
+#endif
+
 #define barrier()	   __asm__ __volatile__ ("" : : : "memory")
 
 #define ACCESS_ONCE(x) (*(volatile __typeof(x) *)&(x))

src/no-op.c
@@ -31,19 +31,19 @@ void __libc_fini_array(void)
 {
 }
 
-void _close(void)
+void __weak _close(void)
 {
 }
 
-void _lseek(void)
+void __weak _lseek(void)
 {
 }
 
-void _read(void)
+void __weak _read(void)
 {
 }
 
-void _write(void)
+void __weak _write(void)
 {
 }
 
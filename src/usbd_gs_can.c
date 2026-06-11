candle-usb/candleLight_fw

commit 0080001f288439cfb96fbae8fe4832767f5234a8

usbd_gs_can: USBD_GS_CAN_SendToHost(): return untransmitted message back to transmit queue, rather than free queue
Fixes: f418aa362c5c ("usbd_gs_can: clean up send to host path")

src/usbd_gs_can.c
@@ -897,7 +897,7 @@ void USBD_GS_CAN_SendToHost(USBD_HandleTypeDef *pdev)
 		return;
 
 	was_irq_enabled = disable_irq();
-	list_add(&hcan->to_host_buf->list, &hcan->list_frame_pool);
+	list_add(&hcan->to_host_buf->list, &hcan->list_to_host);
 	hcan->to_host_buf = NULL;
 	restore_irq(was_irq_enabled);
 }

commit 20b6d4498a6057e4231c3434e346bee9c59759b8

usbd_gs_can: USBD_GS_CAN_SendToHost(): fix USBD_GS_CAN_SendFrame() error handling
If USBD_GS_CAN_SendFrame() fails, it will be due to a USB suspend event
(is_usb_suspend_cb == true). For now the firmware shuts down the CAN
interface during suspend and hcan->to_host_buf is pruged by
usbd_gs_can_purge_to_host_buf() during USBD_GS_CAN_DeInit().

Take into account that hcan->to_host_buf might be NULL. If to_host_buf is
set, move it to the hcan->list_frame_pool.

Fixes: 0080001f2884 ("usbd_gs_can: USBD_GS_CAN_SendToHost(): return untransmitted message back to transmit queue, rather than free queue")

src/usbd_gs_can.c
@@ -905,9 +905,18 @@ void USBD_GS_CAN_SendToHost(USBD_HandleTypeDef *pdev)
 	if (result == USBD_OK)
 		return;
 
+	/*
+	 * If USBD_GS_CAN_SendFrame() fails, it will be due to a USB suspend event
+	 * (is_usb_suspend_cb == true).
+	 * For now the firmware shuts down the CAN interface during suspend and
+	 * hcan->to_host_buf is pruged by usbd_gs_can_purge_to_host_buf() during
+	 * USBD_GS_CAN_DeInit().
+	 */
 	was_irq_enabled = disable_irq();
-	list_add(&hcan->to_host_buf->list, &hcan->list_to_host);
-	hcan->to_host_buf = NULL;
+	if (hcan->to_host_buf) {
+		list_add_tail(&hcan->to_host_buf->list, &hcan->list_frame_pool);
+		hcan->to_host_buf = NULL;
+	}
 	restore_irq(was_irq_enabled);
 }
 
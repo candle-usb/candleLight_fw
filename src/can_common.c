123d6d7a171258f43be6fa3bfd081a63d042f486
Marc Kleine-Budde <mkl@blackshift.org>
2026-06-12 22:52:50 +0200

    can_common: can_err_to_state(), gs_can_tx_state_to_frame(), gs_can_rx_state_to_frame(): add CAN state handling helper functions
    These functions allows the firmware to easier create CAN state change
    CAN error frames as expected by the Linux kernel.

--- src/can_common.c
+++ src/can_common.c
@@ -1,6 +1,7 @@
 /*
  * The MIT License (MIT)
  *
+ * Copyright (c) 2026 Marc Kleine-Budde <kernel@pengutronix.de>
  * Copyright (c) 2016 Hubert Denkmair
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -29,6 +30,10 @@
 #include "led.h"
 #include "timer.h"
 #include "usbd_gs_can.h"
+
+#define CAN_ERROR_WARNING_THRESHOLD 96
+#define CAN_ERROR_PASSIVE_THRESHOLD 128
+#define CAN_BUS_OFF_THRESHOLD		256
 
 #ifndef CONFIG_CANFD
 const struct gs_device_bt_const_extended CAN_btconst_ext;
@@ -138,6 +143,46 @@ void CAN_ReceiveFrame(USBD_GS_CAN_HandleTypeDef *hcan, can_data_t *channel)
 	list_add_tail_locked(&frame_object->list, &hcan->list_to_host);
 
 	led_indicate_trx(&channel->leds, LED_RX);
+}
+
+enum gs_can_state can_err_to_state(const uint16_t err)
+{
+	if (err < CAN_ERROR_WARNING_THRESHOLD)
+		return GS_CAN_STATE_ERROR_ACTIVE;
+	if (err < CAN_ERROR_PASSIVE_THRESHOLD)
+		return GS_CAN_STATE_ERROR_WARNING;
+	if (err < CAN_BUS_OFF_THRESHOLD)
+		return GS_CAN_STATE_ERROR_PASSIVE;
+
+	return GS_CAN_STATE_BUS_OFF;
+}
+
+u8 gs_can_tx_state_to_frame(const enum gs_can_state state)
+{
+	switch (state) {
+		case GS_CAN_STATE_ERROR_ACTIVE:
+			return CAN_ERR_CRTL_ACTIVE;
+		case GS_CAN_STATE_ERROR_WARNING:
+			return CAN_ERR_CRTL_TX_WARNING;
+		case GS_CAN_STATE_ERROR_PASSIVE:
+			return CAN_ERR_CRTL_TX_PASSIVE;
+		default:
+			return 0;
+	}
+}
+
+u8 gs_can_rx_state_to_frame(const enum gs_can_state state)
+{
+	switch (state) {
+		case GS_CAN_STATE_ERROR_ACTIVE:
+			return CAN_ERR_CRTL_ACTIVE;
+		case GS_CAN_STATE_ERROR_WARNING:
+			return CAN_ERR_CRTL_RX_WARNING;
+		case GS_CAN_STATE_ERROR_PASSIVE:
+			return CAN_ERR_CRTL_RX_PASSIVE;
+		default:
+			return 0;
+	}
 }
 
 // If there are frames to receive, don't report any error frames. The

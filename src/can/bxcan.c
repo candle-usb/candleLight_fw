candle-usb/candleLight_fw

commit f6f2254cbcd9d283fff6a1cfecb473da2bfe37cb

bxcan: implement GS_CAN_FEATURE_BERR_REPORTING
This patch takes @jhofstee's idea of commit 2d06e4ce4ef8 ("add a flag to
optionally enable berr-reporting") and commit 98af324d7503 ("handle state
changes and bit error reporting seperately") and ports it to current
mainline.

Linux kernel support for GS_CAN_FEATURE_BERR_REPORTING was documented in
commit 1f1835264d81 ("can: gs_usb: document GS_CAN_FEATURE_BERR_REPORTING")
and implemented in commit 2f3cdad1c616 ("can: gs_usb: add ability to enable
/ disable berr reporting").

Link: https://github.com/jhofstee/candleLight_fw/commit/2d06e4ce4ef8ee07fa24e7824b195a02050043d0
Link: https://github.com/jhofstee/candleLight_fw/commit/98af324d75039fc445b80f8e4aecc42ee7e0214c
Link: https://github.com/torvalds/linux/commit/1f1835264d81da03fcd05646542a622678b379d6
Link: https://github.com/torvalds/linux/commit/2f3cdad1c616891ab25ce10c1ac146403605b960
Link: https://github.com/candle-usb/candleLight_fw/issues/154

src/can/bxcan.c
@@ -52,6 +52,7 @@ const struct gs_device_bt_const CAN_btconst = {
 		GS_CAN_FEATURE_PAD_PKTS_TO_MAX_PKT_SIZE |
 		(IS_ENABLED(CONFIG_TERMINATION) ?
 		 GS_CAN_FEATURE_TERMINATION : 0) |
+		GS_CAN_FEATURE_BERR_REPORTING |
 		(IS_ENABLED(CONFIG_CAN_FILTER) ?
 		 GS_CAN_FEATURE_FILTER : 0) |
 		0,
@@ -363,6 +364,10 @@ bool can_drv_bus_error_pending(const struct can_channel *channel)
 {
 	const uint32_t reg_esr = channel->instance->ESR;
 	const uint32_t lec = FIELD_GET(CAN_ESR_LEC, reg_esr);
+
+	if (!(channel->feature & GS_CAN_FEATURE_BERR_REPORTING)) {
+		false;
+	}
 
 	return lec != BXCAN_LEC_NO_ERROR && lec != BXCAN_LEC_SOFTWARE;
 }

commit 562c8ad67f6b6d9c90cdcf70cbbb7c9f5ed193f5

bxcan: can_drv_bus_error_pending(): fix check if bus error reporting is enabled
Fixes: f6f2254cbcd9 ("bxcan: implement GS_CAN_FEATURE_BERR_REPORTING")

src/can/bxcan.c
@@ -372,12 +372,12 @@ void can_drv_get_device_state(const struct can_channel *channel, struct gs_devic
 
 bool can_drv_bus_error_pending(const struct can_channel *channel)
 {
+	if (!(channel->feature & GS_CAN_FEATURE_BERR_REPORTING)) {
+		return false;
+	}
+
 	const uint32_t reg_esr = channel->instance->ESR;
 	const uint32_t lec = FIELD_GET(CAN_ESR_LEC, reg_esr);
-
-	if (!(channel->feature & GS_CAN_FEATURE_BERR_REPORTING)) {
-		false;
-	}
 
 	return lec != BXCAN_LEC_NO_ERROR && lec != BXCAN_LEC_SOFTWARE;
 }
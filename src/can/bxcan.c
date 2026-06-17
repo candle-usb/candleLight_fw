candle-usb/candleLight_fw

commit 5ecbd1694f6d42cd7a54740a123543d9daa53686

can_common, bxcan: improve CAN state handling
This patch takes @jhofstee's idea of commit 98af324d7503 ("handle state
changes and bit error reporting seperately") splits it into separate
patches and ports it current mainline.

Honorable mention to @pgreenland, who implemented something similar in
commit a8b19b1f6230 ("Prevent error frames from being generated and
discarded on each main loop iteration. Checking for changes in error status
which may lead to an error frame being emitted.")

Add `gs_can_state` to `struct can_channel`, to make it easy to report state
changes.

Check if there is something to report at all before claiming and preparing
an error message.

Report report state changes, instead of the state, like other socketcan
interface, see
https://github.com/torvalds/linux/blob/6b5a2b7d9bc156e505f09e698d85d6a1547c1206/drivers/net/can/dev/dev.c#L156-L187

Honorable mention to @pgreenland, who implemented something similar in
commit a8b19b1f6230 ("Prevent error frames from being generated and
discarded on each main loop iteration. Checking for changes in error status
which may lead to an error frame being emitted.")

Link: https://github.com/jhofstee/candleLight_fw/commit/98af324d75039fc445b80f8e4aecc42ee7e0214c
Link: https://github.com/pgreenland/candleLight_fw/commit/a8b19b1f6230dc2dfbab4e80dd40515d42f04561
Link: https://github.com/torvalds/linux/blob/6b5a2b7d9bc156e505f09e698d85d6a1547c1206/drivers/net/can/dev/dev.c#L156-L187

src/can/bxcan.c
@@ -1,6 +1,7 @@
 /*
  * The MIT License (MIT)
  *
+ * Copyright (c) 2026 Marc Kleine-Budde <kernel@pengutronix.de>
  * Copyright (c) 2016 Hubert Denkmair
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -310,6 +311,46 @@ bool can_send(can_data_t *channel, struct gs_host_frame *frame)
 	}
 }
 
+enum gs_can_state can_drv_get_state(const struct can_channel *channel)
+{
+	const uint32_t reg_esr = channel->instance->ESR;
+
+	if (!(reg_esr & (CAN_ESR_BOFF | CAN_ESR_EPVF | CAN_ESR_EWGF))) {
+		return GS_CAN_STATE_ERROR_ACTIVE;
+	}
+
+	if (reg_esr & CAN_ESR_BOFF) {
+		return GS_CAN_STATE_BUS_OFF;
+	}
+
+	if (reg_esr & CAN_ESR_EPVF) {
+		return GS_CAN_STATE_ERROR_PASSIVE;
+	}
+
+	return GS_CAN_STATE_ERROR_WARNING;
+}
+
+void can_drv_handle_state_change(const struct can_channel *channel, struct gs_host_frame *frame)
+{
+	const uint32_t reg_esr = channel->instance->ESR;
+	enum gs_can_state tx_state, rx_state;
+	u8 tx_err, rx_err;
+
+	tx_err = FIELD_GET(CAN_ESR_TEC, reg_esr);
+	rx_err = FIELD_GET(CAN_ESR_REC, reg_esr);
+
+	tx_state = can_err_to_state(tx_err);
+	rx_state = can_err_to_state(rx_err);
+
+	if (tx_state >= rx_state)
+		frame->classic_can->data[1] |= gs_can_tx_state_to_frame(tx_state);
+	if (tx_state <= rx_state)
+		frame->classic_can->data[1] |= gs_can_rx_state_to_frame(rx_state);
+
+	frame->classic_can->data[6] = tx_err;
+	frame->classic_can->data[7] = rx_err;
+}
+
 uint32_t can_get_error_status(can_data_t *channel)
 {
 	CAN_TypeDef *can = channel->instance;
@@ -321,22 +362,14 @@ uint32_t can_get_error_status(can_data_t *channel)
 	return err;
 }
 
-static bool status_is_active(uint32_t err)
+bool can_parse_error_status(can_data_t __maybe_unused *channel, struct gs_host_frame *frame, uint32_t err)
 {
-	return !(err & (CAN_ESR_BOFF | CAN_ESR_EPVF));
-}
-
-bool can_parse_error_status(can_data_t *channel, struct gs_host_frame *frame, uint32_t err)
-{
-	uint32_t last_err = channel->reg_esr_old;
 	/*
 	 * We build up the detailed error information at the same time as we decide
 	 * whether there's anything worth sending. This variable tracks that final
 	 * result.
 	 */
 	bool should_send = false;
-
-	channel->reg_esr_old = err;
 
 	frame->echo_id = 0xFFFFFFFF;
 	frame->can_id  = CAN_ERR_FLAG;
@@ -350,28 +383,6 @@ bool can_parse_error_status(can_data_t *channel, struct gs_host_frame *frame, ui
 	frame->classic_can->data[6] = 0;
 	frame->classic_can->data[7] = 0;
 
-	if (err & CAN_ESR_BOFF) {
-		if (!(last_err & CAN_ESR_BOFF)) {
-			/* We transitioned to bus-off. */
-			frame->can_id |= CAN_ERR_BUSOFF;
-			should_send = true;
-		}
-		// - tec (overflowed) / rec (looping, likely used for recessive counting)
-		//   are not valid in the bus-off state.
-		// - The warning flags remains set, error passive will cleared.
-		// - LEC errors will be reported, while the device isn't even allowed to send.
-		//
-		// Hence only report bus-off, ignore everything else.
-		return should_send;
-	}
-
-	/* We transitioned from passive/bus-off to active, so report the edge. */
-	if (!status_is_active(last_err) && status_is_active(err)) {
-		frame->can_id |= CAN_ERR_CRTL;
-		frame->classic_can->data[1] |= CAN_ERR_CRTL_ACTIVE;
-		should_send = true;
-	}
-
 	uint8_t tx_error_cnt = (err >> 16) & 0xFF;
 	uint8_t rx_error_cnt = (err >> 24) & 0xFF;
 	/*
@@ -380,20 +391,6 @@ bool can_parse_error_status(can_data_t *channel, struct gs_host_frame *frame, ui
 	 */
 	frame->classic_can->data[6] = tx_error_cnt;
 	frame->classic_can->data[7] = rx_error_cnt;
-
-	if (err & CAN_ESR_EPVF) {
-		if (!(last_err & CAN_ESR_EPVF)) {
-			frame->can_id |= CAN_ERR_CRTL;
-			frame->classic_can->data[1] |= CAN_ERR_CRTL_RX_PASSIVE | CAN_ERR_CRTL_TX_PASSIVE;
-			should_send = true;
-		}
-	} else if (err & CAN_ESR_EWGF) {
-		if (!(last_err & CAN_ESR_EWGF)) {
-			frame->can_id |= CAN_ERR_CRTL;
-			frame->classic_can->data[1] |= CAN_ERR_CRTL_RX_WARNING | CAN_ERR_CRTL_TX_WARNING;
-			should_send = true;
-		}
-	}
 
 	uint8_t lec = (err>>4) & 0x07;
 	switch (lec) {

include/can.h
@@ -41,7 +41,7 @@ typedef struct can_channel {
 	struct list_head list_from_host;
 	led_data_t leds;
 	uint32_t feature;
-	uint32_t reg_esr_old;
+	enum gs_can_state state;
 #if defined (CONFIG_BXCAN)
 	struct gs_device_filter filter;
 	uint32_t btr;

include/can_drv.h
@@ -28,6 +28,10 @@
 #include <stdint.h>
 
 struct can_channel;
+struct gs_host_frame;
 
 void can_drv_enable(struct can_channel *channel);
 void can_drv_disable(struct can_channel *channel);
+
+enum gs_can_state can_drv_get_state(const struct can_channel *channel);
+void can_drv_handle_state_change(const struct can_channel *channel, struct gs_host_frame *frame);

src/can_common.c
@@ -74,6 +74,7 @@ void can_enable(struct can_channel *channel, const uint32_t feature)
 	channel->feature = feature;
 
 	led_set_mode(&channel->leds, LED_MODE_NORMAL);
+	channel->state = GS_CAN_STATE_ERROR_ACTIVE;
 	can_drv_enable(channel);
 }
 
@@ -81,6 +82,7 @@ void can_disable(USBD_GS_CAN_HandleTypeDef *hcan, struct can_channel *channel)
 {
 	can_drv_disable(channel);
 	usbd_gs_can_purge_to_host_list_by_channel(hcan, channel);
+	channel->state = GS_CAN_STATE_STOPPED;
 	led_set_mode(&channel->leds, LED_MODE_OFF);
 }
 
@@ -202,6 +204,34 @@ u8 gs_can_rx_state_to_frame(const enum gs_can_state state)
 	}
 }
 
+void can_handle_state_change(USBD_GS_CAN_HandleTypeDef *hcan, struct can_channel *channel)
+{
+	enum gs_can_state new_state;
+
+	new_state = can_drv_get_state(channel);
+
+	if (new_state == channel->state)
+		return;
+
+	channel->state = new_state;
+
+	struct gs_host_frame_object *frame_object = gs_host_frame_object_get_locked(hcan);
+	if (!frame_object)
+		return;
+
+	struct gs_host_frame *frame = &frame_object->frame;
+	can_prepare_error_frame(channel, frame);
+
+	if (channel->state == GS_CAN_STATE_BUS_OFF) {
+		frame->can_id |= CAN_ERR_BUSOFF;
+	} else {
+		frame->can_id |= CAN_ERR_CRTL | CAN_ERR_CNT;
+		can_drv_handle_state_change(channel, frame);
+	}
+
+	list_add_tail_locked(&frame_object->list, &hcan->list_to_host);
+}
+
 // If there are frames to receive, don't report any error frames. The
 // best we can localize the errors to is "after the last successfully
 // received frame", so wait until we get there. LEC will hold some error
@@ -237,4 +267,6 @@ void CAN_HandleError(USBD_GS_CAN_HandleTypeDef *hcan, can_data_t *channel)
 	} else {
 		list_add_tail_locked(&frame_object->list, &hcan->list_frame_pool);
 	}
+
+	can_handle_state_change(hcan, channel);
 }

commit 8d859d78f9cc1604db134254c6efb71cd65113d2

bx_can: can_drv_get_state(): return current state if stopped or sleeping
During a `modprobe -r gs_usb; modprobe gs_usb` cycle the following spurious
CAN error frame is generated:

```
 (2026-06-17 16:47:16.592988)     can0  RX - -  20000204   {8}  00 40 00 00 00 00 00 00   ERRORFRAME
        controller-problem{back-to-error-active}
        error-counter-tx-rx{{0}{0}}
```

Since commit 5ecbd1694f6d ("can_common, bxcan: improve CAN state
handling"), when the firmware disables the interface it sets `struct
can_channel::state` to `GS_CAN_STATE_STOPPED`.

The `main()` loop is still running, `can_handle_state_change()` will detect
a state transition from `GS_CAN_STATE_STOPPED` to the actual CAN state,
usually `GS_CAN_STATE_ERROR_ACTIVE` and generate a CAN state error message.

Fix the error by returning the current state if stopped or sleeping.

Fixes: 5ecbd1694f6d ("can_common, bxcan: improve CAN state handling")

src/can/bxcan.c
@@ -323,6 +323,9 @@ bool can_send(can_data_t *channel, struct gs_host_frame *frame)
 
 enum gs_can_state can_drv_get_state(const struct can_channel *channel)
 {
+	if (channel->state >= GS_CAN_STATE_STOPPED)
+		return channel->state;
+
 	const uint32_t reg_esr = channel->instance->ESR;
 
 	if (!(reg_esr & (CAN_ESR_BOFF | CAN_ESR_EPVF | CAN_ESR_EWGF))) {
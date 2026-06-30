candle-usb/candleLight_fw

commit ba0fb8c71fb1882d14a31a67b60fecf88a6df212

can_common: CAN_HandleError(): read status register once and pass it to called functions

src/can/bxcan.c
@@ -317,12 +317,15 @@ uint32_t can_drv_read_reg_status(const struct can_channel *channel)
 	return channel->instance->ESR;
 }
 
-enum gs_can_state can_drv_get_state(const struct can_channel *channel)
+void can_drv_clear_reg_status(const struct can_channel *channel)
+{
+	channel->instance->ESR |= FIELD_PREP(CAN_ESR_LEC, CAN_LEC_SOFTWARE);
+}
+
+enum gs_can_state can_drv_get_state(const struct can_channel *channel, const uint32_t reg_esr)
 {
 	if (channel->state >= GS_CAN_STATE_STOPPED)
 		return channel->state;
-
-	const uint32_t reg_esr = channel->instance->ESR;
 
 	if (!(reg_esr & (CAN_ESR_BOFF | CAN_ESR_EPVF | CAN_ESR_EWGF))) {
 		return GS_CAN_STATE_ERROR_ACTIVE;
@@ -339,9 +342,9 @@ enum gs_can_state can_drv_get_state(const struct can_channel *channel)
 	return GS_CAN_STATE_ERROR_WARNING;
 }
 
-void can_drv_handle_state_change(const struct can_channel *channel, struct gs_host_frame *frame)
+void can_drv_handle_state_change(const struct can_channel __maybe_unused *channel, struct gs_host_frame *frame,
+								 const uint32_t reg_esr)
 {
-	const uint32_t reg_esr = channel->instance->ESR;
 	enum gs_can_state tx_state, rx_state;
 	u8 tx_err, rx_err;
 
@@ -360,31 +363,28 @@ void can_drv_handle_state_change(const struct can_channel *channel, struct gs_ho
 	frame->classic_can->data[7] = rx_err;
 }
 
-void can_drv_get_device_state(const struct can_channel *channel, struct gs_device_state *state)
+void can_drv_get_device_state(const struct can_channel *channel, struct gs_device_state *state,
+							  const uint32_t reg_esr)
 {
-	const uint32_t reg_esr = channel->instance->ESR;
-
-	state->state = can_drv_get_state(channel);
+	state->state = can_drv_get_state(channel, reg_esr);
 	state->rxerr = FIELD_GET(CAN_ESR_REC, reg_esr);
 	state->txerr = FIELD_GET(CAN_ESR_TEC, reg_esr);
 }
 
-bool can_drv_bus_error_pending(const struct can_channel *channel)
+bool can_drv_bus_error_pending(const struct can_channel *channel, const uint32_t reg_esr)
 {
 	if (!(channel->feature & GS_CAN_FEATURE_BERR_REPORTING)) {
 		return false;
 	}
 
-	const uint32_t reg_esr = channel->instance->ESR;
 	const uint32_t lec = FIELD_GET(CAN_ESR_LEC, reg_esr);
 
 	return can_is_lec_error(lec);
 }
 
-void can_drv_handle_bus_error(const struct can_channel *channel, struct gs_host_frame *frame)
+void can_drv_handle_bus_error(const struct can_channel __maybe_unused *channel, struct gs_host_frame *frame,
+							  const uint32_t reg_esr)
 {
-	const uint32_t reg_esr = channel->instance->ESR;
-
 	frame->can_id |= CAN_ERR_PROT | CAN_ERR_BUSERROR | CAN_ERR_CNT;
 
 	can_lec_error_to_frame(frame, FIELD_GET(CAN_ESR_LEC, reg_esr));

include/can_drv.h
@@ -36,9 +36,9 @@ void can_drv_enable(struct can_channel *channel);
 void can_drv_disable(struct can_channel *channel);
 
 uint32_t can_drv_read_reg_status(const struct can_channel *channel);
-enum gs_can_state can_drv_get_state(const struct can_channel *channel);
-void can_drv_handle_state_change(const struct can_channel *channel, struct gs_host_frame *frame);
-void can_drv_get_device_state(const struct can_channel *channel, struct gs_device_state *state);
+enum gs_can_state can_drv_get_state(const struct can_channel *channel, const uint32_t reg);
+void can_drv_handle_state_change(const struct can_channel *channel, struct gs_host_frame *frame, const uint32_t reg);
+void can_drv_get_device_state(const struct can_channel *channel, struct gs_device_state *state, const uint32_t reg);
 
-bool can_drv_bus_error_pending(const struct can_channel *channel);
-void can_drv_handle_bus_error(const struct can_channel *channel, struct gs_host_frame *frame);
+bool can_drv_bus_error_pending(const struct can_channel *channel, const uint32_t reg);
+void can_drv_handle_bus_error(const struct can_channel *channel, struct gs_host_frame *frame, const uint32_t reg);

src/can_common.c
@@ -151,7 +151,9 @@ void CAN_ReceiveFrame(USBD_GS_CAN_HandleTypeDef *hcan, can_data_t *channel)
 
 void can_get_device_state(const struct can_channel *channel, struct gs_device_state *state)
 {
-	can_drv_get_device_state(channel, state);
+	const uint32_t reg_status = can_drv_read_reg_status(channel);
+
+	can_drv_get_device_state(channel, state, reg_status);
 }
 
 static void can_prepare_error_frame(const struct can_channel *channel,
@@ -235,9 +237,10 @@ void can_lec_error_to_frame(struct gs_host_frame *frame, const uint8_t lec)
 	}
 }
 
-static void can_handle_bus_error(USBD_GS_CAN_HandleTypeDef *hcan, struct can_channel *channel)
+static void can_handle_bus_error(USBD_GS_CAN_HandleTypeDef *hcan, struct can_channel *channel,
+								 const uint32_t reg_status)
 {
-	if (!can_drv_bus_error_pending(channel))
+	if (!can_drv_bus_error_pending(channel, reg_status))
 		return;
 
 	struct gs_host_frame_object *frame_object = gs_host_frame_object_get_locked(hcan);
@@ -247,16 +250,17 @@ static void can_handle_bus_error(USBD_GS_CAN_HandleTypeDef *hcan, struct can_cha
 	struct gs_host_frame *frame = &frame_object->frame;
 
 	can_prepare_error_frame(channel, frame);
-	can_drv_handle_bus_error(channel, frame);
+	can_drv_handle_bus_error(channel, frame, reg_status);
 
 	list_add_tail_locked(&frame_object->list, &hcan->list_to_host);
 }
 
-static void can_handle_state_change(USBD_GS_CAN_HandleTypeDef *hcan, struct can_channel *channel)
+static void can_handle_state_change(USBD_GS_CAN_HandleTypeDef *hcan, struct can_channel *channel,
+									const uint32_t reg_status)
 {
 	enum gs_can_state new_state;
 
-	new_state = can_drv_get_state(channel);
+	new_state = can_drv_get_state(channel, reg_status);
 
 	if (new_state == channel->state)
 		return;
@@ -274,7 +278,7 @@ static void can_handle_state_change(USBD_GS_CAN_HandleTypeDef *hcan, struct can_
 		frame->can_id |= CAN_ERR_BUSOFF;
 	} else {
 		frame->can_id |= CAN_ERR_CRTL | CAN_ERR_CNT;
-		can_drv_handle_state_change(channel, frame);
+		can_drv_handle_state_change(channel, frame, reg_status);
 	}
 
 	list_add_tail_locked(&frame_object->list, &hcan->list_to_host);
@@ -290,6 +294,7 @@ void CAN_HandleError(USBD_GS_CAN_HandleTypeDef *hcan, can_data_t *channel)
 		return;
 	}
 
-	can_handle_bus_error(hcan, channel);
-	can_handle_state_change(hcan, channel);
+	const uint32_t reg_status = can_drv_read_reg_status(channel);
+	can_handle_bus_error(hcan, channel, reg_status);
+	can_handle_state_change(hcan, channel, reg_status);
 }

commit 295d17a34259d37d78afa2b5671904222d7e6ed9

bxcan: can_drv_clear_reg_status(): remove accidentally added function
Fixes: ba0fb8c71fb1 ("can_common: CAN_HandleError(): read status register once and pass it to called functions")

src/can/bxcan.c
@@ -317,11 +317,6 @@ uint32_t can_drv_read_reg_status(const struct can_channel *channel)
 	return channel->instance->ESR;
 }
 
-void can_drv_clear_reg_status(const struct can_channel *channel)
-{
-	channel->instance->ESR |= FIELD_PREP(CAN_ESR_LEC, CAN_LEC_SOFTWARE);
-}
-
 enum gs_can_state can_drv_get_state(const uint32_t reg_esr)
 {
 	if (!(reg_esr & (CAN_ESR_BOFF | CAN_ESR_EPVF | CAN_ESR_EWGF))) {
14454f7ccb741f6dae228e25baffcde9ec6cb160
Marc Kleine-Budde <mkl@blackshift.org>
2026-07-04 21:26:06 +0200

    usbd_conf: uncrustify

--- libs/STM32_USB_Device_Library/config/usbd_conf.h
+++ libs/STM32_USB_Device_Library/config/usbd_conf.h
@@ -28,11 +28,11 @@ THE SOFTWARE.
 
 #include "hal_include.h"
 
-#define USBD_MAX_NUM_INTERFACES       1
-#define USBD_MAX_NUM_CONFIGURATION    1
-#define USBD_DESC_BUF_SIZE            192
+#define USBD_MAX_NUM_INTERFACES		  1
+#define USBD_MAX_NUM_CONFIGURATION	  1
+#define USBD_DESC_BUF_SIZE			  192
 #define USBD_SUPPORT_USER_STRING_DESC 1
-#define USBD_SELF_POWERED             0
-#define DEVICE_FS                     0
+#define USBD_SELF_POWERED			  0
+#define DEVICE_FS					  0
 
 #define USBD_ErrLog(...)
